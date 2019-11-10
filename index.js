7693fe12207ca3e9078def7bce4be68d1da05368
soniabhijeet94 <soniabhijeet94@yahoo.com>
2019-11-11 01:55:38 +0530

    Implementing word_definition - in progress..

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
-const args = process.argv;
+const args = process.argv.slice(2);
+const api_host = 'https://fourtytwowords.herokuapp.com';
+const word_api = api_host + 'word/';
+const words_api = api_host + 'words/';
+const api_key = 'b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c112627073481104fec2fb1a0cc9d84c2212474c0cbe7d8e59d7b95c7cb32a1133f778abd1857bf934ba06647fda4f59e878d164';
 
-let helpCommands = () => {
+var fortyTwoWordsApi = (url, callback) => {
+	//setup the calling to DB to fetch required results
+};
+
+var helpCommands = () => {
   console.log('Available Commands:');
   console.log('\t1.dict defn <word>');
   console.log('\t2.dict syn <word>');
@@ -14,11 +22,40 @@ let helpCommands = () => {
   console.log('\t8.dict help');
 };
 
+var definitionsAll = (word, callback) => {
+  let url = '';
+
+  //{apihost}/word/{word}/definitions?api_key={api_key} =? for Word Definition
+  let route = `${word}/definitions?api_key=${api_key}`;
+  url = word_api + route;
+  fortyTwoWordsApi(url, (data) => {
+    callback(data);
+  });
+};
+
+var showDefinition = (word) => {
+	definitionsAll(word, (data) => {
+		if(data.length >= 1){
+		  console.log(`The definitions for the word ${word} are : `);
+		  console.log(data)
+		}
+	});
+}
+
 
 var initDictionary = () => {
 
-	if(args[2].toLowerCase() === "help")
-		helpCommands();
+	// console.log(process.argv);
+
+	//For no args, call wordOfTheDay api
+	if(args.length === 2 && args[0] === "defn"){
+
+		let word = args[1];
+	    showDefinition(word);
+ 	}
+
+	// if(args[2].toLowerCase() === "help")
+	// 	helpCommands();
 
 }
 
